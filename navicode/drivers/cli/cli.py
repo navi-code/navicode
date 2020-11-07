67e6c1546fa2f07422c5188af7424ff5cc6c75cc
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2020-11-07 17:24:23 +0530

    Add query option

--- navicode/drivers/cli/cli.py
+++ navicode/drivers/cli/cli.py
@@ -37,10 +37,7 @@ def navicode_init():
             comments = comment_parser(python_file)
             filename = python_file[python_file.index(dirname):]
             for comment in comments:
-                if filename in comments_dump.keys():
-                    comments_dump[filename].append(str(len(corpus)) + '---' + str(comment[1]))
-                else:
-                    comments_dump[filename] = [str(len(corpus)) + '---' + str(comment[1])]
+                comments_dump[len(corpus)] = str(filename) + '---' + str(comment[1])
 
                 corpus.append(comment[0])
 
@@ -63,7 +60,34 @@ def navicode_query():
         print("\033[91mError: NaviCode not initialized, use 'navicode --init' to initialize!")
         return
 
+    dirname = os.path.basename(cur_dir)
+
     print("\nInitializing model . . .")
 
     embedder = SentenceTransformer('distilbert-base-nli-stsb-mean-tokens')
+
+    print("\nLoading comment embeddings and lookup JSON . . .")
+
+    corpus_embeddings = torch.load(os.path.join(navi_dir, dirname + '_navi.emb'))
+
+    with open(os.path.join(navi_dir, dirname + '_navi.json'), 'r') as file:
+        lookup = json.load(file)
+
+    while True:
+        query = input(">>> Enter your query (exit to quit): ")
+        if query == "exit":
+            break
+
+        query_embedding = embedder.encode(query, convert_to_tensor=True)
+        cos_scores = util.pytorch_cos_sim(query_embedding, corpus_embeddings)[0]
+        cos_scores = cos_scores.cpu()
+
+        topk = 3
+        scores, idxs = torch.topk(cos_scores, k=topk)
+
+        print(f"\nTop-{topk} matches are:\n")
+
+        for idx in idxs:
+            filename, line_num = lookup[str(idx.item())].split("---")
+            print(f"Match found in file - {filename} at line number around - {line_num}")
 
